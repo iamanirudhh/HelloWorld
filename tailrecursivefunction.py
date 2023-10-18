3e861c18c9859631e1fb505852eacc1ef7a7294e
Anirudh Diware <147695621+iamanirudhh@users.noreply.github.com>
2023-10-18 22:42:40 +0530

    Create tailrecursivefunction.py

---
+++ tailrecursivefunction.py
@@ -0,0 +1,12 @@
+# A tail recursive function to 
+# calculate n th fibonacci number
+def fib(n, a = 0, b = 1):
+	if n == 0:
+		return a
+	if n == 1:
+		return b
+	return fib(n - 1, b, a + b);
+
+# Driver Code
+n = 9;
+print("fib("+str(n)+") = "+str(fib(n)))
